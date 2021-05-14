863ba6c075e7679e4a8560d967896acd42a81ebd
Mackenzie Chisholm <chisholm.mackenzie@gmail.com>
2021-05-14 16:51:07 -0400

    updated session variable

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: process.env.SECRET,
+  cookie: { maxAge: 600000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
