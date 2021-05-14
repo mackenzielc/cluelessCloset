482a1e0879375254005445de506745887a162b5d
Mackenzie Chisholm <chisholm.mackenzie@gmail.com>
2021-05-14 17:06:06 -0400

    updated session variable

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: 'super secret',
   cookie: { maxAge: 600000 },
   resave: false,
   saveUninitialized: true,
